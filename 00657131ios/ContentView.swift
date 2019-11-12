0ff4156e84cfa1cfd04a98a1b399cd5fe4b4b734
Wilhelm Shen <33245687+WilhelmIllust@users.noreply.github.com>
2019-11-12 13:06:03 +0800

    Initial Commit

---
+++ 00657131ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00657131ios
+//
+//  Created by User11 on 2019/11/12.
+//  Copyright © 2019 alice. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
